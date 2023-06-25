4a7eb5f82e06ad91b66854f91f8d10641284666e
Lino <linolam26@gmail.com>
2023-06-25 11:03:16 +0200

    Your Message

---
+++ python1.py
@@ -0,0 +1,15 @@
+print("tian you duo da")
+L = []
+S = 0
+for loop in range(1, 100):
+    L.append(loop)
+
+
+for loop in range(len(L)):
+    S += L[loop]
+
+
+print(L, S)
+
+yasuo = 2
+print(yasuo)
